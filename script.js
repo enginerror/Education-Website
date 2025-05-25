f6198f56c5c316156111473f5c6a7660f008dc68
Ayaan Murshed Khan <ayaanmurshedkhan@gmail.com>
2025-05-25 23:28:15 +0530

    complete design part

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ faqs.forEach(faq => {
         }
     })
 })
+
+// smooth effect
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElement.forEach((el) => observer.observe(el))
